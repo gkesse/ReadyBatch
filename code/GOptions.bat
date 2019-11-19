9b092c2335446cec119bcb93f223b946ff349b34
Gerard KESSE <tiakagerard@hotmail.com>
2019-11-19 08:30:12 +0100

    Initial Commit

--- code/GOptions.bat
+++ code/GOptions.bat
@@ -4,9 +4,9 @@ call %*
 goto :eof
 ::===============================================
 :GetArgs
-    ::call GString :ToUpper "%1" lProcess
+    call GString :ToUpper %1 lProcess
     
-    call GConfig :SetData PROCESS %1
+    call GConfig :SetData PROCESS %lProcess%
     call GConfig :SetData OPTION_1 %2
     call GConfig :SetData OPTION_2 %3
 goto :eof
