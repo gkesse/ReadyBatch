d7062494721aa9bb6c154ebf5f7bcb8f57bd75fe
Gerard KESSE <tiakagerard@hotmail.com>
2019-11-21 03:57:41 +0100

    Initial Commit

---
+++ code/GBatch.bat
@@ -0,0 +1,93 @@
+@echo off 
+::===============================================
+set "GBATCH_TEST_MAP="
+set "GBATCH_TEST_MAP=%GBATCH_TEST_MAP%;CharScape"
+set "GBATCH_TEST_MAP=%GBATCH_TEST_MAP%;CommandMultiLine"
+set "GBATCH_TEST_MAP=%GBATCH_TEST_MAP%;EchoScape"
+::===============================================
+call %*
+goto :eof
+::===============================================
+:Test
+    call GConfig :GetData OPTION_1 lOption1
+    if "%lOption1%" == "number" ( goto TestNumber
+    ) else ( if "%lOption1%" == "run" ( goto TestRun
+    ) else ( if "%lOption1%" == "last" ( goto TestLast
+    ) else ( if "%lOption1%" == "number" ( goto TestNumber
+    ) else ( if "%lOption1%" == "all" ( goto TestAll
+    ) else ( goto TestNumber )))))
+goto :eof
+::===============================================
+:TestNumber
+setlocal enabledelayedexpansion
+    set /a lTestCount=0
+    for %%A in (%GBATCH_TEST_MAP%) do ( 
+        set /a lTestCount=!lTestCount! + 1
+        echo !lTestCount! : %%A
+    )
+endlocal
+goto :eof
+::===============================================
+:TestRun
+    call GConfig :GetData OPTION_2 lOption2
+setlocal enabledelayedexpansion
+    set /a lTestCount=0
+    for %%A in (%GBATCH_TEST_MAP%) do ( 
+        set /a lTestCount=!lTestCount! + 1
+        if "!lTestCount!" == "%lOption2%" ( goto %%A )
+    )
+endlocal
+goto :eof
+::===============================================
+:TestLast
+    call :TestCount lTestCount
+    call GConfig :SetData OPTION_2 %lTestCount%
+    call :TestRun
+goto :eof
+::===============================================
+:TestAll
+setlocal enabledelayedexpansion
+    set /a lTestCount=0
+    for %%A in (%GBATCH_TEST_MAP%) do ( 
+        set /a lTestCount=!lTestCount! + 1
+        call :%%A
+        echo.
+    )
+endlocal
+goto :eof
+::===============================================
+:TestCount
+setlocal enabledelayedexpansion
+    set /a lTestCount=0
+    for %%A in (%GBATCH_TEST_MAP%) do ( 
+        set /a lTestCount=!lTestCount! + 1
+    )
+endlocal & (
+    set %1=%lTestCount%
+)
+goto :eof
+::===============================================
+:CharScape
+    echo #------------------------------------------------
+    echo ### CharScape
+    echo #------------------------------------------------
+    echo ^& ^| ^^ ^< ^> ^( ^) %%
+goto :eof
+::===============================================
+:CommandMultiLine
+    echo #------------------------------------------------
+    echo ### CommandMultiLine
+    echo #------------------------------------------------
+echo Voici ^
+une commande ^
+multiligne
+goto :eof
+::===============================================
+:EchoScape
+    echo #------------------------------------------------
+    echo ### EchoScape
+    echo #------------------------------------------------
+    echo.
+    echo. /?  Print this help.
+goto :eof
+::===============================================
