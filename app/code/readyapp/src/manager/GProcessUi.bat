15344506f47477932c8d86e4c156df757026d5a9
Gerard KESSE <tiakagerard@hotmail.com>
2021-04-20 17:12:24 +0200

    Initial Commit

--- app/code/readyapp/src/manager/GProcessUi.bat
+++ app/code/readyapp/src/manager/GProcessUi.bat
@@ -6,22 +6,42 @@ goto :eof
 :Run
     set "G_STATE=S_INIT"   
 :RunLoop
-    if "%G_STATE%" == "S_INIT" ( goto Init %*
-    ) else ( if "%G_STATE%" == "S_METHOD" ( goto Method %*
-    ) else ( if "%G_STATE%" == "S_CHOICE" ( goto Method %*
-    ) else ( if "%G_STATE%" == "S_SQLITE" ( goto Method %*
-    ) else ( if "%G_STATE%" == "S_SAVE" ( goto Method %*
-    ) else ( if "%G_STATE%" == "S_LOAD" ( goto Method %*
+    if "%G_STATE%" == "S_INIT" ( goto INIT %*
+    ) else ( if "%G_STATE%" == "S_METHOD" ( goto METHOD %*
+    ) else ( if "%G_STATE%" == "S_CHOICE" ( goto CHOICE %*
+    ) else ( if "%G_STATE%" == "S_SQLITE" ( goto SQLITE %*
+    ) else ( if "%G_STATE%" == "S_SAVE" ( goto SAVE %*
+    ) else ( if "%G_STATE%" == "S_LOAD" ( goto LOAD %*
     ) else ( goto :eof ))))))
 goto :RunLoop
 ::===============================================
-:Init
-    echo Init...
-    set "G_STATE=S_METHOD"
+:INIT
+    echo INIT...
+    set "G_STATE=S_LOAD"
 goto :RunLoop
 ::===============================================
-:Method
-    echo Method...
+:METHOD
+    echo METHOD...
+    set "G_STATE=S_CHOICE"
+goto :RunLoop
+::===============================================
+:CHOICE
+    echo CHOICE...
+    set "G_STATE=S_SQLITE"
+goto :RunLoop
+::===============================================
+:SQLITE
+    echo SQLITE...
+    set "G_STATE=S_SAVE"
+goto :RunLoop
+::===============================================
+:SAVE
+    echo SAVE...
     set "G_STATE=S_END"
+goto :RunLoop
+::===============================================
+:LOAD
+    echo LOAD...
+    set "G_STATE=S_METHOD"
 goto :RunLoop
 ::===============================================
